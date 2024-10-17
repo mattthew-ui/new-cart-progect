9df5f12d942f6bc711c28b5fc68a5892fc84dc82
mattthew-ui <MatthewBerryhill22@outlook.com>
2024-10-17 09:47:20 -0400

    added more js code

--- storebuying.js
+++ storebuying.js
@@ -5,7 +5,15 @@ for(var i = 0; i < removeButton.length; i++){
     button.addEventListener('click',function(event){
         var buttonHit = event.target
         buttonHit.parentElement.parentElement.remove()
+        howMuchIsMytoal()
         console.log(hit)
 
     })
+}
+function howMuchIsMytoal(){
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRow = cartItemContainer.getElementsByClassName('cart-row')
+    for(var i = 0; i < cartRow.length; i++){
+        var cartRow = cartRows[i]
+    }
 }
