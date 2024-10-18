82865520f2cd8f57090b7d2a8a1485240692a666
mattthew-ui <MatthewBerryhill22@outlook.com>
2024-10-17 21:11:15 -0400

    added add to cart fetuers

--- storebuying.js
+++ storebuying.js
@@ -16,6 +16,11 @@ function ready() {
     for (let input of quantityInputs) {
         input.addEventListener('change', quantityChanged);
     }
+    var addToCartButtons = document.getElementsByClassName('shop-item-button')
+    for (var i = 0; i < addToCartButtons.length; i++){
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
 }
 
 // If you want to run this code on load, you can do this:
@@ -23,6 +28,33 @@ if (document.readyState !== 'loading') {
     ready();
 }
 
+
+function addToCartClicked(event){
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    var pictuer = shopItem.getElementsByClassName('shop-item-image')[0].src
+    console.log(title, price , pictuer)
+    addingToCart(title, price , pictuer)
+}
+function addingToCart(title, price , pictuer){
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    var itemsInCart = document.getElementsByClassName('cart-items')[0]
+    var cartRowItems = `
+     <div class="cart-item cart-column">
+        <img class="cart-item-image" src="${pictuer}" width="100" height="100">
+        <span class="cart-item-title">${title}</span>
+    </div>
+    <span class="cart-price cart-column">${price}</span>
+    <div class="cart-quantity cart-column">
+        <input class="cart-quantity-input" type="number" value="1">
+        <button class="btn btn-danger" type="button">REMOVE</button>
+        </div>`
+        cartRow.innerHTML = cartRowItems
+    itemsInCart.append(cartRow)
+}
 
 function howMuchIsMyTotal() {
     var cartItemContainer = document.getElementsByClassName('cart-items')[0];
